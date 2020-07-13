56c139ef69b8b57f4bcb66701e03308b176c578c
Reid Efford <reidefford@gmail.com>
2020-07-13 18:42:49 -0400

    listening

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const port = process.env.PORT || 3000
 app.get('', (req, res) => {
     res.send('hello from node')
 })
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
