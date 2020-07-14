922967c1b90f445720ee9d1e52de9a1c33fa0642
Reid Efford <reidefford@gmail.com>
2020-07-13 22:08:52 -0400

    general query & query with name

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require('dotenv').config();
 
 const { sf_user, sf_password, sf_token } = process.env
 
+console.log('sf_user', sf_user)
+console.log('sf_password', sf_password)
+console.log('sf_token', sf_token)
+
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -12,56 +16,93 @@ const conn = new sf.Connection({
     loginUrl : 'https://login.salesforce.com'
 })
 
-conn.login(sf_user, sf_password + sf_token, error => {
-    if (error) { return console.error(err); }
-    // Now you can get the access token and instance URL information.
-    // Save them to establish connection next time.
-    console.log(conn.accessToken);
-    console.log(conn.instanceUrl);
-    // logged in user property
-    console.log("User ID: " + userInfo.id);
-    console.log("Org ID: " + userInfo.organizationId);
-    // ...
-  });
+app.get('/contact/:id', async (req, res) => {
 
-app.get('/contact/:id', (req, res) => {
-    res.send('hello from node')
+    try{
+        const userInfo = await conn.login(sf_user, sf_password + sf_token)
 
-    const _id  = req.params.id 
+        // Now you can get the access token and instance URL information.
+        // Save them to establish connection next time.
+        console.log(conn.accessToken);
+        console.log(conn.instanceUrl);
+        // logged in user property
+        console.log('logged in')
+        console.log("User ID: " + userInfo.id);
+        console.log("Org ID: " + userInfo.organizationId);
 
-    const soql = `
-        SELECT Id, Name
-        FROM Contact
-        WHERE Id =: ${_id};`
+        console.log('getting data')
 
-    conn.query(soql, (err, result) => {
-        if (err) {
-            res.sendStatus(500);
-        } else if (result.records.length === 0) {
-            res.status(404).send('Session not found.');
-        } else {
+        const id = req.params.id
+        console.log('id passed')
+        console.log(id)
 
-            const formattedData = result.records.map(r => {
-                let speakers = [];
-                
-                speakers = r.records.map(
-                    record => {
-                        return {
-                            id: record.Id,
-                            name: record.Name
-                        };
-                    }
-                );
-            });
-            res.send({ data: formattedData });
-        }
-    });
-
+        const soql = `SELECT Id, Name FROM Contact WHERE Name LIKE '%${id}%'`
     
+        const cons = await conn.query(soql) 
 
+        console.log(cons)
+
+        contact = cons.records.map(item => {
+            return {
+                Id: item.Id,
+                Name: item.Name,
+                
+            }
+        })
+
+        res.send({ data: cons });
+    
+    } catch(error) {
+
+        console.log('something went wrong')
+        console.error(error)
+    }
+    
 })
 
+app.get('', async (req, res) => {
 
+    try{
+        const userInfo = await conn.login(sf_user, sf_password + sf_token)
+
+        // Now you can get the access token and instance URL information.
+        // Save them to establish connection next time.
+        console.log(conn.accessToken);
+        console.log(conn.instanceUrl);
+        // logged in user property
+        console.log('logged in')
+        console.log("User ID: " + userInfo.id);
+        console.log("Org ID: " + userInfo.organizationId);
+
+        console.log('getting data')
+
+        const id = req.params.id
+        console.log('id passed')
+        console.log(id)
+
+        const soql = `SELECT Id, Name, Developer_Level__c FROM Contact WHERE Developer_Level__c != null LIMIT 5`
+    
+        const cons = await conn.query(soql) 
+
+        console.log(cons)
+
+        contact = cons.records.map(item => {
+            return {
+                Id: item.Id,
+                Name: item.Name,
+                Developer_Level__c: item.Developer_Level__c
+            }
+        })
+
+        res.send({ data: contact });
+    
+    } catch(error) {
+
+        console.log('something went wrong')
+        console.error(error)
+    }
+    
+})
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
