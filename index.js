77d0592b682e8aaf2781c48e09846278750c7a34
Reid Efford <reidefford@gmail.com>
2020-07-13 20:50:28 -0400

    updated endpoint and fetching data

--- index.js
+++ index.js
@@ -1,11 +1,66 @@
 const express  = require('express')
+const sf = require('node-salesforce')
+require('dotenv').config();
+
+const { sf_user, sf_password, sf_token } = process.env
+
 const app = express()
 
 const port = process.env.PORT || 3000
 
-app.get('', (req, res) => {
-    res.send('hello from node')
+const conn = new sf.Connection({
+    loginUrl : 'https://login.salesforce.com'
 })
+
+conn.login(sf_user, sf_password + sf_token, error => {
+    if (error) { return console.error(err); }
+    // Now you can get the access token and instance URL information.
+    // Save them to establish connection next time.
+    console.log(conn.accessToken);
+    console.log(conn.instanceUrl);
+    // logged in user property
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+    // ...
+  });
+
+app.get('/contact/:id', (req, res) => {
+    res.send('hello from node')
+
+    const _id  = req.params.id 
+
+    const soql = `
+        SELECT Id, Name
+        FROM Contact
+        WHERE Id =: ${_id};`
+
+    conn.query(soql, (err, result) => {
+        if (err) {
+            res.sendStatus(500);
+        } else if (result.records.length === 0) {
+            res.status(404).send('Session not found.');
+        } else {
+            /* Work with result data */
+        }
+    });
+
+    const formattedData = result.records.map(r => {
+        let speakers = [];
+        
+        speakers = r.records.map(
+            record => {
+                return {
+                    id: r.Id,
+                    name: r.Name
+                };
+            }
+        );
+    });
+    res.send({ data: formattedData });
+
+})
+
+
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
